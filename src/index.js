e46d8b975f637445f8585954830fdeee96840873
KirTheCat <124335988+KirTheCat@users.noreply.github.com>
2024-10-15 10:17:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+import store from './store';
 import theme from './mui/theme';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +10,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode
     </React.StrictMode>
 );
 
